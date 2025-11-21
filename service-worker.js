7414fa34c126ecdcfb78ec1a20bb92dd6e0721a4
Viswamantra <Vikastur@gmail.com>
2025-11-21 13:44:48 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('oshiro').then(c=>c.addAll(['/','/offline.html'])))});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match('/offline.html')))});
