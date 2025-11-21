3f4d7ca4455e6c39d7dcfca9bebc8073078b5dc2
Viswamantra <Vikastur@gmail.com>
2025-11-21 12:51:46 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('oshiro').then(c=>c.addAll(['/','/offline.html'])))});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match('/offline.html')))});
