dc7ad092061d6ad2d1e4f18c787d368a74a584f3
Viswamantra <Vikastur@gmail.com>
2025-11-23 16:08:27 +0530

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+// Import Firebase scripts
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js");
+
+// Your Firebase config
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+});
+
+// Background message handler
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icons/icon-192x192.png",
+  });
+});
