fc1acd6f521b136d0213748bf4e04840037979b8
Viswamantra <Vikastur@gmail.com>
2025-11-24 19:10:00 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,3 +1,13 @@
+import { useEffect, useState } from "react";
+import { requestFirebaseToken, onMessageListener } from "./firebase";
 export default function App() {
   return <h1>Oshiro PWA Running</h1>;
 }
+<button
+  onClick={async () => {
+    const token = await requestFirebaseToken();
+    alert("Your token:\n" + token);
+  }}
+>
+  Enable Notifications
+</button>
