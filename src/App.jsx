abb707752cc923e810f498c6063a2ba97f603e01
Viswamantra <Vikastur@gmail.com>
2025-11-25 20:52:57 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NotificationPanel from "./NotificationPanel";
+
+function App() {
+  return (
+    <div>
+      <h1>Oshiro PWA Running</h1>
+      <NotificationPanel />
+    </div>
+  );
+}
+
+export default App;
