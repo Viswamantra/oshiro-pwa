7b08523303c6e30796ebe933102b38985816e763
Viswamantra <Vikastur@gmail.com>
2025-11-25 22:48:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './pages/App.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./pages/App.jsx";
+
+// Register SW for Firebase Messaging
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/firebase-messaging-sw.js")
     .then(() => console.log("Firebase SW registered"))
-    .catch((err) => console.error("SW error:", err));
+    .catch((err) => console.error("SW registration failed:", err));
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
