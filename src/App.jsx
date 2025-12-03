8f135772bcf313891fb0795d270062f36cb9b3e5
Viswamantra <Vikastur@gmail.com>
2025-12-03 16:12:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import NotificationPanel from "./NotificationPanel";
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import HomeScreen from "./pages/HomeScreen.jsx";
+import MerchantDashboard from "./pages/MerchantDashboard.jsx";
+import HeaderBar from "./components/layout/HeaderBar.jsx";
+import BottomNav from "./components/layout/BottomNav.jsx";
 
 function App() {
   return (
-    <div>
-      <h1>Oshiro PWA Running</h1>
-      <NotificationPanel />
-    </div>
+    <Router>
+      <div className="app-shell">
+        <HeaderBar />
+        <main className="app-main">
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/merchant" element={<MerchantDashboard />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+        <BottomNav />
+      </div>
+    </Router>
   );
 }
 
