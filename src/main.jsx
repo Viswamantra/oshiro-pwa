3c9bd51a06aac5d58bb73145748f26471e73e93e
Viswamantra <Vikastur@gmail.com>
2025-12-03 21:16:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";    // ← only this allowed
+import "./global.css";    // ← only this allowed
 import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
