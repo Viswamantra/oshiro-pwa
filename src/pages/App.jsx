28673070cac714506a3853e7fbbacd7423f9b446
Viswamantra <Vikastur@gmail.com>
2025-12-04 16:53:22 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,57 +1,29 @@
-// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-// Pages
-import Login from "./pages/Login.jsx";
 import HomeScreen from "./pages/HomeScreen.jsx";
 import MerchantDashboard from "./pages/MerchantDashboard.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
+import Login from "./pages/Login.jsx";
+import SelectRole from "./pages/SelectRole.jsx";
 
 function App() {
-  const role = localStorage.getItem("logged_role");
-
   return (
     <BrowserRouter>
       <Routes>
+        {/* Login */}
+        <Route path="/" element={<Login />} />
 
-        {/* LOGIN */}
-        <Route path="/login" element={<Login />} />
+        {/* Role Select */}
+        <Route path="/select-role" element={<SelectRole />} />
 
-        {/* CUSTOMER HOME */}
-        <Route
-          path="/home"
-          element={
-            role === "customer" ? <HomeScreen /> : <Navigate to="/login" />
-          }
-        />
+        {/* Customer Side */}
+        <Route path="/home" element={<HomeScreen />} />
 
-        {/* MERCHANT DASHBOARD */}
-        <Route
-          path="/merchant"
-          element={
-            role === "merchant" ? (
-              <MerchantDashboard />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
+        {/* Merchant Side */}
+        <Route path="/merchant" element={<MerchantDashboard />} />
 
-        {/* ADMIN DASHBOARD */}
-        <Route
-          path="/admin"
-          element={
-            role === "admin" ? (
-              <AdminDashboard />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-
-        {/* DEFAULT ROUTE */}
-        <Route path="*" element={<Navigate to="/login" />} />
+        {/* Admin Side */}
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
