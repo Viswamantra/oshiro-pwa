c5d747e7db2c9dc60ead99e1f14e2be75cdb94f9
Viswamantra <Vikastur@gmail.com>
2025-12-25 17:50:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import LoginPage from "./pages/LoginPage";
-import MerchantDashboard from "./pages/MerchantDashboard";
-import CustomerDashboard from "./pages/CustomerDashboard";
-import AdminDashboard from "./pages/admin/AdminDashboard";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-
-      <Route path="/admin-dashboard" element={<AdminDashboard />} />
-      <Route path="/merchant-dashboard" element={<MerchantDashboard />} />
-      <Route path="/customer-dashboard" element={<CustomerDashboard />} />
-
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    <div style={{ padding: 40 }}>
+      <h1>OshirO App is LIVE</h1>
+      <p>If you see this, the app is fixed.</p>
+    </div>
   );
 }
