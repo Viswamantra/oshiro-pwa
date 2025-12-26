012a70a172d03751344c4d137d9283ab9009e18d
Viswamantra <Vikastur@gmail.com>
2025-12-26 11:49:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import LoginPage from "./pages/LoginPage";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
 
 export default function App() {
-  return <LoginPage />;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
 }
