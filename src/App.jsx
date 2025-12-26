718a2f5a979b5d8ee2a9c7654bfaa47d7bd6b71d
Viswamantra <Vikastur@gmail.com>
2025-12-26 11:25:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,6 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
 import LoginPage from "./pages/LoginPage";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import MerchantDashboard from "./pages/MerchantDashboard";
-import CustomerDashboard from "./pages/CustomerDashboard";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-
-      <Route path="/admin-dashboard" element={<AdminDashboard />} />
-      <Route path="/merchant-dashboard" element={<MerchantDashboard />} />
-      <Route path="/customer-dashboard" element={<CustomerDashboard />} />
-
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
-  );
+  return <LoginPage />;
 }
