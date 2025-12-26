8428e4d1dd847d9aa7505dcd6b8ddf0726247547
Viswamantra <Vikastur@gmail.com>
2025-12-26 12:38:46 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,3 +3,8 @@ import Login from "../Login";
 export default function LoginPage() {
   return <Login />;
 }
+import Login from "../auth/Login";
+
+export default function LoginPage() {
+  return <Login />;
+}
