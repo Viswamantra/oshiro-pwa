614441715307bab85a93cc5c2c5faf77a3203241
Viswamantra <Vikastur@gmail.com>
2025-12-30 20:42:06 +0530

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,18 +1,23 @@
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js"
+);
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBekN6ULTaosrBQzv-JvBlnMcCOMXZ-_JU",
+  authDomain: "oshiro-app.firebaseapp.com",
+  projectId: "oshiro-app",
+  storageBucket: "oshiro-app.firebasestorage.app",
+  messagingSenderId: "1066886336420",
+  appId: "1:1066886336420:web:458379909954c206917b31",
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("🔔 Background message:", payload);
+  console.log("🔔 Background message received:", payload);
 
   self.registration.showNotification(
     payload.notification?.title || "OshirO Alert",
