4d93680b1ab3077fd60534794ee55e0295664cc4
Viswamantra <Vikastur@gmail.com>
2025-12-30 19:35:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js")
+      .then(() => console.log("✅ Firebase SW registered"))
+      .catch((err) => console.error("❌ Firebase SW error", err));
+  });
+}
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
