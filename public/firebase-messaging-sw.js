fff314112b7087ab723ced446d3be755795ca125
Viswamantra <Vikastur@gmail.com>
2025-12-30 19:33:54 +0530

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("🔔 Background message:", payload);
+
+  self.registration.showNotification(
+    payload.notification?.title || "OshirO Alert",
+    {
+      body: payload.notification?.body,
+      icon: "/logo192.png",
+    }
+  );
+});
