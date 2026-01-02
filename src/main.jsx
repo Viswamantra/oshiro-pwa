824a41007c60c1d02cc71eecee3ad82e3bc80765
Viswamantra <Vikastur@gmail.com>
2026-01-02 19:07:17 +0530

    Update main.jsx
    Fix login routing and remove duplicate login sources

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-/* 🔥 FORCE-KILL ALL SERVICE WORKERS */
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => {
-      registration.unregister();
-    });
-  });
-}
-
 createRoot(document.getElementById("root")).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
