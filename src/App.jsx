3a7f2ca3c31e5858faf0672f712269a61a7cf648
Viswamantra <Vikastur@gmail.com>
2026-01-02 13:20:31 +0530

    Update App.jsx
    Fix App.jsx login import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
 
 /* ✅ CORRECT IMPORTS (MATCH YOUR FOLDER STRUCTURE) */
-import Login from "./auth/Login";
 import MerchantDashboard from "./pages/MerchantDashboard";
 import CustomerDashboard from "./pages/CustomerDashboard";
-
 import MerchantRoute from "./routes/MerchantRoute";
 
 export default function App() {
