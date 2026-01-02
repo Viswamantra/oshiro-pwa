91450a6c205d9109c530d97e550c94489a318f99
Viswamantra <Vikastur@gmail.com>
2026-01-02 19:12:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./auth/AuthContext";
-
 import Login from "./auth/Login";
 import MerchantDashboard from "./pages/MerchantDashboard";
 import CustomerDashboard from "./pages/CustomerDashboard";
 
-import MerchantRoute from "./routes/MerchantRoute";
-import CustomerRoute from "./routes/CustomerRoute";
-
 export default function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
 
-        {/* ✅ SINGLE LOGIN SOURCE */}
-        <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
 
-        <Route
-          path="/merchant"
-          element={
-            <MerchantRoute>
-              <MerchantDashboard />
-            </MerchantRoute>
-          }
-        />
+      {/* 🚫 TEMP: NO ROUTE GUARDS */}
+      <Route path="/merchant" element={<MerchantDashboard />} />
+      <Route path="/customer" element={<CustomerDashboard />} />
 
-        <Route
-          path="/customer"
-          element={
-            <CustomerRoute>
-              <CustomerDashboard />
-            </CustomerRoute>
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </AuthProvider>
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 }
