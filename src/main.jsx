c2615c5f415c3c66daaeabd26e62d6f841a00afb
Viswamantra <Vikastur@gmail.com>
2026-01-02 20:26:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+/* =========================
+   APP BOOTSTRAP
+========================= */
+const root = document.getElementById("root");
+
+createRoot(root).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
