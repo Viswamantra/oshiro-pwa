3f7663cb633367ab96734c9070ba46efac5bc0f4
Viswamantra <Vikastur@gmail.com>
2026-01-28 20:52:30 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,8 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 export default function ProtectedRoute({ adminOnly }) {
   const isAdmin = localStorage.getItem("admin") === "true";
 
+  // Admin-only protection
   if (adminOnly && !isAdmin) {
-    return <Navigate to="/admin/login" replace />;
+    return <Navigate to="/admin-login" replace />;
   }
 
   return <Outlet />;
